044cc846828524f904caa264e6cf0a80ab76bba0
Ricardo Perrone <rperrones@yahoo.com>
2021-10-30 14:57:49 -0300

    Added method to get image id
    Method to get unique identification

--- libs/coco_io.py
+++ libs/coco_io.py
@@ -11,10 +11,17 @@ from pycocotools.coco import COCO
 
 COCO_EXT = '.json'
 ENCODE_METHOD = DEFAULT_ENCODING
-COCO_BASIC_FORMAT ={"info","image","annotation","license"}
-COCO_ANNOTATION_FORMAT = {"id","image_id","category_id","segmentation","area","bbox","iscrowd"}
+COCO_BASIC_FORMAT_OBJD = ["info","images","annotations","categories","license"]
+COCO_ANNOTATION_FORMAT = ["id","image_id","category_id","segmentation","area","bbox","iscrowd"]
+COCO_IMAGE_FORMAT = ["id","width","height","file_name","license","flickr_url","coco_url","date_captured"]
 
-class CocoWriter:
+class CocoWriter(COCO):
     def __init__(self, ann_file):
-        self.coco = COCO(ann_file)
+        COCO.__init__(self, ann_file)        
         
+    def get_image_id(self, basename):
+        id = None
+        for image in self.dataset[COCO_BASIC_FORMAT_OBJD[1]]:
+            if image.get(COCO_IMAGE_FORMAT[3]) == basename:
+                id = image.get(COCO_IMAGE_FORMAT[0])
+        return id
