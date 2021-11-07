c893369055e2ee3c19228f673ffaad6e69ecb07a
Ricardo Perrone <rperrones@yahoo.com>
2021-11-07 12:24:26 -0300

    Changed init method to accept a dataset
    - Changed the method signature to accept an already existing dataset

--- libs/coco_io.py
+++ libs/coco_io.py
@@ -8,6 +8,8 @@ e-mail: rperrones@yahoo.com
 """
 from libs.constants import DEFAULT_ENCODING
 from pycocotools.coco import COCO
+from collections import defaultdict
+import time
 
 COCO_EXT = '.json'
 ENCODE_METHOD = DEFAULT_ENCODING
@@ -16,8 +18,22 @@ COCO_ANNOTATION_FORMAT = ["id","image_id","category_id","segmentation","area","b
 COCO_IMAGE_FORMAT = ["id","width","height","file_name","license","flickr_url","coco_url","date_captured"]
 
 class CocoWriter(COCO):
-    def __init__(self, ann_file):
-        COCO.__init__(self, ann_file)        
+    def __init__(self, dataset):
+        """
+        This version was adapted from pycocotools - Constructor of Microsoft COCO helper class for reading and visualizing annotations.
+        :param annotation_file (str): location of annotation file
+        :param image_folder (str): location to the folder that hosts images.
+        :return:
+        """
+        # load dataset
+        self.dataset,self.anns,self.cats,self.imgs = dict(),dict(),dict(),dict()
+        self.imgToAnns, self.catToImgs = defaultdict(list), defaultdict(list)
+        print('loading annotations into memory...')
+        tic = time.time()
+        assert type(dataset)==dict, 'annotation file format {} not supported'.format(type(dataset))
+        print('Done (t={:0.2f}s)'.format(time.time()- tic))
+        self.dataset = dataset
+        self.createIndex()
         
     def get_image_id(self, basename):
         id = None
